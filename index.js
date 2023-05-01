d1fbb5fab89b6aa558d62d519330b2dd5ccc65fc
Pronoy <pronoydas463@gmail.com>
2023-05-01 23:28:20 +0600

    Send the Chef Recipe Data

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const port = 5000;
 
+const chefRecipeData = require("./Data.json");
+
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Indian Best Chef Server is running now !!!");
+});
+
+app.get("/chefRecipe", (req, res) => {
+  res.send(chefRecipeData);
 });
 
 app.listen(port, () => {
